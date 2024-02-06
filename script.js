d57a003d1f555e43eb3bbc232b9eff57f1f24cf9
TalBale <pirty43555@gmail.com>
2024-02-06 10:19:38 +0100

    10:19.06.02.2024

---
+++ script.js
@@ -0,0 +1,17 @@
+function _1task() {
+  $("#addHtmlt").html("<h1> New Element </h1>");
+}
+function _2task() {
+  $(".class").hide();
+}
+function _3task() {
+  $("section").remove();
+}
+function _4task() {
+  $("div:last").addClass("addClass").text("Lumos Maxima");
+}
+
+_1task();
+_2task();
+_3task();
+_4task();
