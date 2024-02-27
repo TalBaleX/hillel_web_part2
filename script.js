dfc5b2349d43cbdaf4fc0c2f5cf51e7fd26f5bce
TalBale <pirty43555@gmail.com>
2024-02-27 08:48:47 +0100

    08:48.27.02.2024

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-let nasaButton = document.querySelector("#nasaButton");
-let esaButton = document.querySelector("#esaButton");
-let jaxaButton = document.querySelector("#jaxaButton");
+let agencyButtons = Array.from(document.getElementsByClassName("agencyButton"));
 
 let astronautsContainer = document.querySelector("#astronautsBox");
 
@@ -13,58 +11,23 @@ fetch("https://api.spacexdata.com/v4/crew")
   .then((astronauts) => {
     astronautsGlobal = astronauts;
   });
-
-nasaButton.addEventListener("click", function () {
-  astronautsContainer.innerHTML = "";
-  astronautsGlobal.forEach((astronaut) => {
-    if (astronaut.agency === "NASA") {
-      astronautsContainer.innerHTML += `
+agencyButtons.forEach((element) => {
+  element.addEventListener("click", function (event) {
+    astronautsContainer.innerHTML = "";
+    astronautsGlobal.forEach((astronaut) => {
+      if (astronaut.agency === event.target.innerText) {
+        astronautsContainer.innerHTML += `
       <div class="card col-3 mt-5" style="width: 18rem;">
         <img src="${astronaut.image}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${astronaut.name}</h5>
           <p class="card-text">Agency: ${astronaut.agency}<br>
           Wikipedia: <a>${astronaut.wikipedia}</a> </p>
-          <a href="#" class="d-block btn btn-primary mt-auto">Go somewhere</a>
+          <a href="${astronaut.wikipedia}" class="btn btn-primary mt-auto">Check his:her wiki</a>
         </div>
       </div>
       `;
-    }
-  });
-});
-esaButton.addEventListener("click", function () {
-  astronautsContainer.innerHTML = "";
-  astronautsGlobal.forEach((astronaut) => {
-    if (astronaut.agency === "ESA") {
-      astronautsContainer.innerHTML += `
-      <div class="card col-3 mt-5" style="width: 18rem;">
-        <img src="${astronaut.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${astronaut.name}</h5>
-          <p class="card-text">Agency: ${astronaut.agency}<br>
-          Wikipedia: <a>${astronaut.wikipedia}</a> </p>
-          <a href="#" class="btn btn-primary mt-auto">Go somewhere</a>
-        </div>
-      </div>
-      `;
-    }
-  });
-});
-jaxaButton.addEventListener("click", function () {
-  astronautsContainer.innerHTML = "";
-  astronautsGlobal.forEach((astronaut) => {
-    if (astronaut.agency === "JAXA") {
-      astronautsContainer.innerHTML += `
-      <div class="card col-3 mt-5" style="width: 18rem;">
-        <img src="${astronaut.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${astronaut.name}</h5>
-          <p class="card-text">Agency: ${astronaut.agency}<br>
-          Wikipedia: <a>${astronaut.wikipedia}</a> </p>
-          <a href="#" class="btn btn-primary mt-auto">Go somewhere</a>
-        </div>
-      </div>
-      `;
-    }
+      }
+    });
   });
 });
